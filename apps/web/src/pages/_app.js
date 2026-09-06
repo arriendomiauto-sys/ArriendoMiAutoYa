938226b49cbcf1ca834cd28ce01cd1bafe22692b
arriendomiauto-sys <arriendomiauto145@gmail.com>
2026-09-06 18:59:06 -0300

    feat: integrate Google Analytics 4 (GA4) with measurement ID G-RR34T24BTE

--- apps/web/src/pages/_app.js
+++ apps/web/src/pages/_app.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.css";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "G-RR34T24BTE";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,7 +13,33 @@ export default function App({ Component, pageProps }) {
           content="width=device-width, initial-scale=1, maximum-scale=5"
         />
       </Head>
+
+      {/* Google Analytics 4 (GA4) */}
+      {GA_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
+
       <Component {...pageProps} />
     </>
   );
 }
+
